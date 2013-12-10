de9f7a3600f22503cfd75cd9ff7cef5e167dfadd
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 13:39:46 -0500

    industry_expressions script page/ all about how many toe socks I have

---
+++ Git Repository/Expressions/Expressions Assignment/Expressions _Wacky/js/script.js
@@ -0,0 +1,6 @@
+//Expressions_Wacky
+//Amy Harvey 12/10/2013
+
+//how many pairs and single toe socks can i come up with?
+
+var toeSocks = [22, 16, 3, 25] //I've got 22 single toe socks in the dryer, 16 pairs in the dresser, found 3 in the basket and 25 more in the washer
