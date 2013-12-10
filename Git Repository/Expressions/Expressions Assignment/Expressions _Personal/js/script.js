cdf837b116b088053e25b25bc41b041cd41ba0ef
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 12:30:45 -0500

    prompt for baseballs in a bucket

--- Git Repository/Expressions/Expressions Assignment/Expressions _Personal/js/script.js
+++ Git Repository/Expressions/Expressions Assignment/Expressions _Personal/js/script.js
@@ -2,3 +2,4 @@
 //Amy Harvey 12/10/2013 Expression_Personal
 
 //How many baseballs will the kids get to hit in tryouts based off the amount of balls and buckets we have at the park
+var baseBalls = prompt("How many Baseballs are in the bucket?") //want user to prompt how many baseballs are in the bucket
