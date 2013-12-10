707d327fc6a2b70848b9635fd72237ca45073a0c
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 12:21:18 -0500

    committing the beginning to expressions_personal js file

---
+++ Git Repository/Expressions/Expressions Assignment/Expressions _Personal/js/script.js
@@ -0,0 +1,6 @@
+//Personal Expression
+//Amy Harvey 12/10/2013 Expression_Personal
+
+var baseBalls = prompt("How many Baseballs are in the bucket?") //want user to prompt how many baseballs are in the bucket
+var bucketOballs = prompt("How many buckets did you bring to the park?") //user prompt for how many buckets total they brought to the park today
+var kidsPlaying = prompt("How many kids signed up to play Baseball?") //user prompt for how many kids want to play baseball
