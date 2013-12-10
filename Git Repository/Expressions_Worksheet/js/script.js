7d96ec17da65d49572e2629df1ca0d019fb59029
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 10:46:29 -0500

    submitting for the expression worksheet up to the grocery average

--- Git Repository/Expressions_Worksheet/js/script.js
+++ Git Repository/Expressions_Worksheet/js/script.js
@@ -30,3 +30,13 @@ var remainder = 48 % 20;
 console.log("sparky got" + " " + remainder + " " + "slices of pizza")
 
 
+// array for average of grocery totals
+const groceryTotal = [25.62, 62.75, 45.89, 40.09, 55.99]
+// the amount for all grocery totals
+var total = groceryTotal[0] + groceryTotal[1] + groceryTotal[2] + groceryTotal[3] + groceryTotal[4]
+
+var avg = total / 5;
+
+console.log("you have spent a total of" + " " + total + " " + "on groceries over 5 weeks. That is an average of" + " " + avg + " " + "per week")
+
+
