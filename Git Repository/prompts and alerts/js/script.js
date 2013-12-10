b592270049141db1b2450b01d518201f87768f5e
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 11:38:12 -0500

    area of triangle determined through prompts

--- Git Repository/prompts and alerts/js/script.js
+++ Git Repository/prompts and alerts/js/script.js
@@ -1,4 +1,6 @@
 //Prompts and Alerts
 
-var userInput = prompt("Enter Your year of birth");
-console.log(userInput)
+var width = prompt("we are calculating the area of a rectangle. \nPlease enter the width.")
+var height = prompt("Please enter the height")
+var area = width * height; //calculates information given to print into the console
+console.log(area)
