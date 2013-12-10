6ea0877ebeca703940b499aeb58c729dc2371ed6
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 09:35:41 -0500

    answer to slice of pie part 1

--- Git Repository/Expressions_Worksheet/js/script.js
+++ Git Repository/Expressions_Worksheet/js/script.js
@@ -12,3 +12,7 @@ console.log(age) //console.log is equal to human years times dog years
 var pizzaSlices = 8; //slices per pizza
 var partyPeople = 24; //amount of people at the party
 var pizzaBought = 6; // total pizzas for the party
+
+var result = pizzaBought * pizzaSlices / partyPeople;
+
+console.log(result)
