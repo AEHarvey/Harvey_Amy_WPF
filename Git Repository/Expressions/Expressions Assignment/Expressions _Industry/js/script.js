9808999e634b5d5124353311e263090388d8e995
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 13:19:26 -0500

    var landscape for how many pics are in landscape mode for expression_industry

--- Git Repository/Expressions/Expressions Assignment/Expressions _Industry/js/script.js
+++ Git Repository/Expressions/Expressions Assignment/Expressions _Industry/js/script.js
@@ -4,3 +4,4 @@
 
 var websitePics = 567; //the amount of pictures setup to go on a designed website
 var bwPics = 210; // the amount of pictures in black and white
+var landsPics = 99 // the amount of pictures photographed in landscape mode
