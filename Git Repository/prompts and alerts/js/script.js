9252dfddad5f5750c571b79d1f501bd72d7f97d3
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 11:41:57 -0500

    using alerts to give you the answers you were inputting into the prompts

--- Git Repository/prompts and alerts/js/script.js
+++ Git Repository/prompts and alerts/js/script.js
@@ -3,4 +3,7 @@
 var width = prompt("we are calculating the area of a rectangle. \nPlease enter the width.")
 var height = prompt("Please enter the height")
 var area = width * height; //calculates information given to print into the console
-console.log(area)
+//console.log(area) //prints to the console
+
+var result = "The area of your rectangle is" + " " + area + " " + "square feet"
+alert(result);
