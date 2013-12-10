22b6dd842c0d7b495eb444989cfc786d310bc335
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 09:30:50 -0500

    beginning of slice of pie part 1

--- Git Repository/Expressions_Worksheet/js/script.js
+++ Git Repository/Expressions_Worksheet/js/script.js
@@ -8,3 +8,7 @@ var age = sparkysAge * 7; //multiplying dog years by human years
 console.log(age) //console.log is equal to human years times dog years
 
 //Slice of pie part 1
+
+var pizzaSlices = 8; //slices per pizza
+var partyPeople = 24; //amount of people at the party
+var pizzaBought = 6; // total pizzas for the party
