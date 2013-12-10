41b69e1f06065fcc4477e6a17bdec0215494f08e
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-09 20:46:11 -0500

    order of operations

--- Git Repository/Expressions/Modulo/js/script.js
+++ Git Repository/Expressions/Modulo/js/script.js
@@ -1,4 +1,11 @@
-//Expressions - Modulo
+//Expressions - PEMDAS - Order of Operations
 
-var remainder = 4 % 10;
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of all the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average)
