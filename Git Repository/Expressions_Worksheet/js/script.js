143c34d4877d1a8d7054be9f161b028db39afa71
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 11:15:52 -0500

    completion of the expression assignment

--- Git Repository/Expressions_Worksheet/js/script.js
+++ Git Repository/Expressions_Worksheet/js/script.js
@@ -35,8 +35,38 @@ const groceryTotal = [25.62, 62.75, 45.89, 40.09, 55.99]
 // the amount for all grocery totals
 var total = groceryTotal[0] + groceryTotal[1] + groceryTotal[2] + groceryTotal[3] + groceryTotal[4]
 
-var avg = total / 5;
+var avg = total / 5; //average total spent over 5 weeks at the grocery store
 
 console.log("you have spent a total of" + " " + total + " " + "on groceries over 5 weeks. That is an average of" + " " + avg + " " + "per week")
+
+
+//amount of an item after discount given before and after tax
+
+const origPrice = 284.75; //original price of item is 284.75
+var discoGiven = .30;  //discount given is 30 percent of original price
+var backMsg = 284.75 * .30; //item is a back massage for your work chair
+var salesTax = .07; //sales tax is 7% on the dollar
+var totalPT = origPrice - backMsg; // total of discount given pre tax
+var totalSt = origPrice * salesTax; // the amount of sales tax on original price of item
+var totalAll = origPrice + totalSt; //the amount of item with sales tax
+var discoGave = totalAll * discoGiven; // discount given with sales tax added on
+var totalEnd = totalAll - discoGave; //item price after discount
+
+console.log("Your Back Massager was originally" + " " + "$" + origPrice + " " + ", but after the" + " " + discoGiven + " " + "discount, it is now" + " " + "$" + totalPT + " " + "without tax, and" + " " + "$" + totalEnd + " " + "with tax")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
