bcc676253f2cd6117f46c04b6e8062fd84e49654
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-09 21:11:39 -0500

    javascript page for expressions worksheet/dogyears problem answered and checked

--- Git Repository/Expressions_Worksheet/js/script.js
+++ Git Repository/Expressions_Worksheet/js/script.js
@@ -1,1 +1,9 @@
 //Amy Harvey 12/07/2013 Expressions Worksheet
+
+//Dog years problem
+
+var sparkysAge = 4; //sparkysage is 4
+var age = sparkysAge * 7; //multiplying dog years by human years
+
+console.log(age) //console.log is equal to human years times dog years
+
