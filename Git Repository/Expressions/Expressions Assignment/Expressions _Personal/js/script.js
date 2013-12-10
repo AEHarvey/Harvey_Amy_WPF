b77bb4509120e337417407eea2f84ccb536dbe77
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-10 12:30:12 -0500

    changed my expression_personal file

--- Git Repository/Expressions/Expressions Assignment/Expressions _Personal/js/script.js
+++ Git Repository/Expressions/Expressions Assignment/Expressions _Personal/js/script.js
@@ -1,6 +1,4 @@
 //Personal Expression
 //Amy Harvey 12/10/2013 Expression_Personal
 
-var baseBalls = prompt("How many Baseballs are in the bucket?") //want user to prompt how many baseballs are in the bucket
-var bucketOballs = prompt("How many buckets did you bring to the park?") //user prompt for how many buckets total they brought to the park today
-var kidsPlaying = prompt("How many kids signed up to play Baseball?") //user prompt for how many kids want to play baseball
+//How many baseballs will the kids get to hit in tryouts based off the amount of balls and buckets we have at the park
