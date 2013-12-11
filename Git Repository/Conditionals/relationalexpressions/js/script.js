8ef8700b473a7244eabaab8a4e1a35052a4e90b2
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-11 10:19:37 -0500

    relational expressions for comparing variables

---
+++ Git Repository/Conditionals/relationalexpressions/js/script.js
@@ -0,0 +1,11 @@
+//Conditional logic - Relational Expressions
+
+var kidHeight = 50;
+
+//if the child is old enough, print to the console "You can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > 48){
+   //code performed if the condition is true
+    console.log("You Can ride the coaster!");
+}
+
