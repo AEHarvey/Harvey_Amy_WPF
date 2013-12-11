314b43f5f1c54013794a5e9cacc2dd55a5d44a87
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-11 10:20:16 -0500

    changing to compare the variables

--- Git Repository/Conditionals/relationalexpressions/js/script.js
+++ Git Repository/Conditionals/relationalexpressions/js/script.js
@@ -1,10 +1,10 @@
 //Conditional logic - Relational Expressions
 
 var kidHeight = 50;
-
+var minHeight = 48;
 //if the child is old enough, print to the console "You can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > 48){
+if(kidHeight > minHeight){
    //code performed if the condition is true
     console.log("You Can ride the coaster!");
 }
