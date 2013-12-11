51c7a035e589d799e420290aa253f278ce8cbfb9
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-11 10:52:59 -0500

    if and else screencast beginning

--- Git Repository/Conditionals/WithExpressions/js/script.js
+++ Git Repository/Conditionals/WithExpressions/js/script.js
@@ -2,11 +2,13 @@
 
 var kidHeight = 47;
 var minHeight = 48;
-var sneakerLift = 2;
+
 //if the child is old enough, print to the console "You can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
    //code performed if the condition is true
     console.log("You Can ride the coaster!");
 }
-
+if(kidHeight < minHeight){
+    console.log("Sorry kid, you've got some growing to do first!")
+}
