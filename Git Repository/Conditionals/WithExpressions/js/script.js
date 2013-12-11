97e42bbea14ad819bbf1572e4aaf7e64769589ee
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-11 10:24:37 -0500

    fixing mistake for the first with expressions commit, removed the + 2 expression

--- Git Repository/Conditionals/WithExpressions/js/script.js
+++ Git Repository/Conditionals/WithExpressions/js/script.js
@@ -4,7 +4,7 @@ var kidHeight = 40;
 var minHeight = 48;
 //if the child is old enough, print to the console "You can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + 2 > minHeight){
+if(kidHeight > minHeight){
    //code performed if the condition is true
     console.log("You Can ride the coaster!");
 }
