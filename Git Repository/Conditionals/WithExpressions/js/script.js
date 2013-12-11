4329aab8bf622e1ae983c6e1cdebfa7a0cce8d67
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-11 10:50:44 -0500

    conditionals with expressions

--- Git Repository/Conditionals/WithExpressions/js/script.js
+++ Git Repository/Conditionals/WithExpressions/js/script.js
@@ -1,10 +1,11 @@
 //Conditional logic - With Expressions
 
-var kidHeight = 40;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 //if the child is old enough, print to the console "You can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
    //code performed if the condition is true
     console.log("You Can ride the coaster!");
 }
