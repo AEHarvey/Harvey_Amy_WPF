e99ed5d89173246adc2147dec68b83691d9f296b
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-13 09:42:35 -0500

    beginning of the "last chance for gas" problem

--- Git Repository/Conditionals/Conditionals_Worksheet/js/script.js
+++ Git Repository/Conditionals/Conditionals_Worksheet/js/script.js
@@ -1,3 +1,12 @@
 //Conditionals Worksheet
 //Amy Harvey 12/13/2013 Conditionals Worksheet
 
+//Last chance for gas!
+
+var gasEfficiency = 32; // gas tank gets 32MPG on the highway
+var gasPercentLeft = .15; // gas tank has 15% left before the car runs out of gas
+var gTankSize = 27; // gas tank can hold 27 gallons
+
+
+
+
