424e478c18d53812d09b8f3172ba82c690162f9c
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-13 08:17:56 -0500

    making var for learning logical operators. being able to buy the phone based on budget

--- Git Repository/Conditionals/Logical_Operators/js/script.js
+++ Git Repository/Conditionals/Logical_Operators/js/script.js
@@ -1,2 +1,12 @@
 //Conditionals Operators - Logical Operators
 
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
+
+//if the price of the phone is less than our budget
+if(iPhonePrice < budget){
+    console.log("We can buy the phone");
+}else{
+    console.log("No phone for you!!");
+}
