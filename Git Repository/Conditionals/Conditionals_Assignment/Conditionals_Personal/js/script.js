d87298bc81426a896882cf600691d45bc148cab7
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-14 10:07:52 -0500

    script for conditional assignment personal problem

---
+++ Git Repository/Conditionals/Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -0,0 +1,21 @@
+//Conditionals Assignment - Conditionals_Personal
+//Amy Harvey 12/13/2013 Conditionals_Personal
+
+
+//christmas lights in and on the house
+var xmasLightsFront = prompt ("How many Christmas lights are out front this year?"); //how many christmas lights do you have out front 
+var xmasLightsBack = prompt ("How many Christmas lights are out back this year?");
+var xmasLightsIn = prompt ("How many Christmas lights total are inside?");
+var totalXmasLights = (+xmasLightsBack + +xmasLightsFront + +xmasLightsIn);
+var xmasLightsOutside = (+xmasLightsBack + +xmasLightsFront);
+
+
+if(totalXmasLights <= 2000){
+    console.log("With" + " " + xmasLightsOutside + " " + "Christmas lights is just enough for you can compete in the contest");
+
+}else if(xmasLightsOutside >=1000){
+    console.log( "With" + " " + xmasLightsOutside + " " + "Christmas lights up, you can compete in the daily contest instead of the weekly contest for best Christmas design");
+
+}else{
+    console.log("You only have" + " " + totalXmasLights + " " + "Christmas lights and that is not enough to compete this year");
+}
