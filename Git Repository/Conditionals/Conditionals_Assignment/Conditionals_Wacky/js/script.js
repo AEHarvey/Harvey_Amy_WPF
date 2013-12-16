39575f5b954d4b6e0b9d162345a3bd9485dd9e5e
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-15 19:38:49 -0500

    script page for conditionals assignment - wacky problem

---
+++ Git Repository/Conditionals/Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -0,0 +1,16 @@
+//Conditionals Assignment - Wacky Problem
+//Amy Harvey 12/14/2013 Conditionals Assignment - Wacky Problem
+
+// Dogs with double dewclaws!
+
+var greatP = 3; // the great pyrenees down the road has 3 dewclaws
+var huskyD = 4; // the husky next door has 4, one on each paw
+var miniPin = 1; // my friends mini pin has 1 dewclaw
+var totalDeclaw = +greatP + +huskyD + +miniPin;
+
+if(totalDeclaw > 10){
+    
+
+}else{
+
+}
