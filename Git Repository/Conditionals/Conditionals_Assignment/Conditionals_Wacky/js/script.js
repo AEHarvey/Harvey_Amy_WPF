01df8c214d64c7aa3e2b3626ec937501f594e03a
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-15 19:48:40 -0500

    commented out the if else statement and added a ternary to do the same thing

--- Git Repository/Conditionals/Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Git Repository/Conditionals/Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -8,9 +8,12 @@ var huskyD = 4; // the husky next door has 4, one on each paw
 var miniPin = 1; // my friends mini pin has 1 dewclaw
 var totalDeclaw = (+greatP + +huskyD + +miniPin); //total amount of dewclaws for the dogs I know
 
-if(totalDeclaw > 10){ // if the total dewclaws between 3 dogs equals 10 it'll print this to the console
+/*if(totalDeclaw > 10){ // if the total dewclaws between 3 dogs equals 10 it'll print this to the console
     console.log("I know 3 people that have dogs that have declaws!"); // sentence it will print if total is above 10
 
 }else{ //else condition if total dewclaws is less than 10
     console.log("I know 3 dogs with dewclaws but they only have" + " " + totalDeclaw + " " + "total declaws"); // what it will print to the console if total dewclaws is lower than 10
-}
+}*/
+// commenting out if else statement to put in ternary
+
+(totalDeclaw > 10) ? console.log("I know 3 people that have dogs that have declaws!") : console.log("I know 3 dogs with dewclaws but they only have" + " " + totalDeclaw + " " + "total declaws");
