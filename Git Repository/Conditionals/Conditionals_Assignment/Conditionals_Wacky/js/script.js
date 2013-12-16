ab61948150513e25b288ee82bc166169ffd2bf30
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-15 19:43:43 -0500

    commenting on first console.log/ what it will print if equals above 10

--- Git Repository/Conditionals/Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Git Repository/Conditionals/Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -8,8 +8,8 @@ var huskyD = 4; // the husky next door has 4, one on each paw
 var miniPin = 1; // my friends mini pin has 1 dewclaw
 var totalDeclaw = (+greatP + +huskyD + +miniPin); //total amount of dewclaws for the dogs I know
 
-if(totalDeclaw > 10){
-    console.log("I know 3 people that have dogs that have declaws!");
+if(totalDeclaw > 10){ // if the total dewclaws between 3 dogs equals 10 it'll print this to the console
+    console.log("I know 3 people that have dogs that have declaws!"); // sentence it will print if total is above 10
 
 }else{
     console.log("I know 3 dogs with dewclaws but they only have" + " " + totalDeclaw + " " + "total declaws");
