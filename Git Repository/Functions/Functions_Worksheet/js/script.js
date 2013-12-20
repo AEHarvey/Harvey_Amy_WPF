bf3c588223c45870badb53ee28077fca02be35be
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-20 12:15:05 -0500

    redoing the functions worksheet, after understanding the return on a function I restarted the problems, input the variable for total

--- Git Repository/Functions/Functions_Worksheet/js/script.js
+++ Git Repository/Functions/Functions_Worksheet/js/script.js
@@ -1,13 +1,7 @@
 //Functions Worsheet
 //Amy Harvey 12/19/2013
 
-//Circumference
-//Calculating the circumference of a circle
+//Circumference Problem
+//Circumference calculation is diameter * Pi
 
-function cirDiameter(circlesize){//diameter of a circle times 2 is the circumference
-    var circumferenceSize = circlesize * 3.14;//circumference is diameter by Pi
-    console.log("The circumeference of the circle is " + circumferenceSize)// console.log print out
-}
-
-cirDiameter(3)
-cirDiameter(2)
+var total = circumfSize(4, 3.14); // arguments sent to parameters
