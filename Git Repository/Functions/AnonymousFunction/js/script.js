08f25989470ea0e768bc01d232e77069265263bc
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-19 22:31:21 -0500

    script for anonmyous function changes/different setup than normal functions

--- Git Repository/Functions/AnonymousFunction/js/script.js
+++ Git Repository/Functions/AnonymousFunction/js/script.js
@@ -2,10 +2,17 @@
 //Amy Harvey 12/19/2013
 
 
-var calcArea = function(width, height){
+
+var calcArea = function(width, height){ //defining
     //code the function runs
     var area = width * height;
     return = area
 }
 
-calcArea(20, 30);
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
+
+
+//anonymous functions are a different setup to standard functions
+//important to know both styles
