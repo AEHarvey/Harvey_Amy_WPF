0edf576e59c41336af5bb1f57ce64dd08d018656
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-19 22:11:52 -0500

    put notes where arguments/parameters go

--- Git Repository/Functions/Function ArgumentsParameters/js/script.js
+++ Git Repository/Functions/Function ArgumentsParameters/js/script.js
@@ -1,9 +1,10 @@
 //Functions - Arguments and Parameters
 //Amy Harvey 12/19/2013
 
-function dogYears(age){
+function dogYears(age){//parameters
     var dogYears = age * 7;
     console.log("Sparky is " + dogYears + " years old.")
 }
-dogYears(4);
+var age1 = 4;
+dogYears(4); //arguments
 dogYears(5);
