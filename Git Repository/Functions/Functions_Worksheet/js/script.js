29332b2605f4d48949b1b05c29dfe2d697258cac
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-19 23:16:30 -0500

    function worksheet// finding the circumference is diameter by pi

---
+++ Git Repository/Functions/Functions_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Functions Worsheet
+//Amy Harvey 12/19/2013
+
+//Circumference
+//Calculating the circumference of a circle
+
+
+
+function cirCircumf(diameter){ //cicumference is diameter times Pi
+    var cirCircumf = 4 * 3.14;
+    console.log("The circumference of the circle is " + cirCircumf)
+}
+cirCircumf(3)
