3a202773063f85a81c028033595255f3373d51c3
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-19 21:52:49 -0500

    removing first function "hello world"

--- Git Repository/Functions/Function Scope/js/script.js
+++ Git Repository/Functions/Function Scope/js/script.js
@@ -2,11 +2,6 @@
 //Amy Harvey 12/19/2013
 
 
-function outptMsg (){
-    console.log("Hello World")
-        //creating a function for output message "hello world"
-}
-
 function calcArea(){
     var width = 20;
     var height = 30;
