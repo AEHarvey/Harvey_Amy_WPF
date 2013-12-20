9e9dc34fede0e49c8d40b199fa6a892b38068577
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-20 12:28:07 -0500

    put in the return function and added console.log so it knows what to print to the console based on the variables and expressions. also changed the pi symbol back to "p" because it was producing errors.

--- Git Repository/Functions/Functions_Worksheet/js/script.js
+++ Git Repository/Functions/Functions_Worksheet/js/script.js
@@ -6,8 +6,9 @@
 
 var total = circumfSize(4, 3.14); // arguments sent to parameters
 
-function circumfSize(d, π){ //parameters from the arguments for functions to use
-    var circleSize = d * π; // variable for circle size, diameter times pi, which are the arguments set in variable total
-    return circleSize;
-    
+function circumfSize(d, p){ //parameters from the arguments for functions to use
+    var circleSize = d * p; // variable for circle size, diameter times pi, which are the arguments set in variable total
+    return circleSize; // return function is 'spitting' out the information it was given
+
+    //also removed the pi symbol because it kept giving me errors. input the 'p' for pi
 }
