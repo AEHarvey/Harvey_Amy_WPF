de233e1b44045f813e912e9c571ac7cef7b87baf
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-19 21:52:05 -0500

    beginning of function scope

---
+++ Git Repository/Functions/Function Scope/js/script.js
@@ -0,0 +1,17 @@
+//Functions - excution
+//Amy Harvey 12/19/2013
+
+
+function outptMsg (){
+    console.log("Hello World")
+        //creating a function for output message "hello world"
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+    //function for output of "area"
+}
+calcArea();
