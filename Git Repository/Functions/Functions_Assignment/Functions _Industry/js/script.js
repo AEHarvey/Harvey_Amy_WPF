d822a8a4bece2ff0774318a5b095bae3ba4e6a2d
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-20 22:05:41 -0500

    industry functions assignment beginning

---
+++ Git Repository/Functions/Functions_Assignment/Functions _Industry/js/script.js
@@ -0,0 +1,10 @@
+//Functions Assignment - Industry Week 4
+//Amy Harvey 12/20/2013
+
+// industry section for assignment
+// webpages needed for a small shopping website
+
+
+function graphicsAmt(){ //input function for the amount of graphics
+
+}
