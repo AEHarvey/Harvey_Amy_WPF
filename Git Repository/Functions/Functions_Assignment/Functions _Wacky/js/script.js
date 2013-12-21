d7c2a15186ab11495ae2f5b890384f1d978d919c
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-21 00:49:48 -0500

    commented out the if else statement and notated. going to put in ternary

--- Git Repository/Functions/Functions_Assignment/Functions _Wacky/js/script.js
+++ Git Repository/Functions/Functions_Assignment/Functions _Wacky/js/script.js
@@ -10,13 +10,15 @@ function getAmts(e, t){ //parameters
     var elvesFeet = e * 2; // amount elves feet on the trip is elvesGo * 2, elves have 2 feet per body
     var totalFeet = +turtleFeet + +elvesFeet; //total amount of feels is turtleFeet + elvesfeet
 
-    if(totalFeet >= 40 || turtleGo >=10 || elvesGo >=10){
+    /*if(totalFeet >= 40 || turtleGo >=10 || elvesGo >=10){
         //code performed if turtles or elves going is 10 or up or total feet is equal or greater than 40
         console.log('Head Turtle says "too many feet to stop at the village salon for a pedicure!"');
             }else{
                 //code performed if there is not too many feet and the village salon will be able to fit us in
                 console.log('Head Elf says "I think the village salon shop worker can fit us in before we continue our journey!"');
-    }
+    }*/
+
+    //commenting out the if else statement to put in ternary
 
 }
 
