4da21d76447fe27c1c2c089af5f4fae366735a58
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-20 23:23:54 -0500

    committed console.log change and notes put up at top for which problem this is

--- Git Repository/Functions/Functions_Assignment/Functions _Personal/js/script.js
+++ Git Repository/Functions/Functions_Assignment/Functions _Personal/js/script.js
@@ -1,15 +1,17 @@
 //Functions Assignment - Personal Week 4
 //Amy Harvey 12/20/2013
 
+//personal function assignment determining the information of my basketball goal
 
 var total = getHeight([], [], []); // var to call function
 
 function getHeight(b, g, b){ //parameters from the arguments for functions to use
     var bball1 = prompt("How many basketballs do you own?"); // prompt to find out how many basketballs owned.
     var bballGoal = prompt("How many feet high is your basketball goal?"); //prompt for how high the basketball goal is
-    var backBoard = prompt("How wide is the backboard") //prompt for how wide the backboard is 
+    var backBoard = prompt("How wide is the backboard") //prompt for how wide the backboard is
 
-
+    console.log("My basketball goal is " + bballGoal + " feet high, I own " + bball1 + " basketball and the backboard is " + backBoard + " feet wide" )
+    //console print for the basketball information prompted and entered.
 }
 
 
