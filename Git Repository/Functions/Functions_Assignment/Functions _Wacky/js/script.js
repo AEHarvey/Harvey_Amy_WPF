63ec16b420845bc03989428311bed194b1a8513c
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-21 00:40:53 -0500

    changes made to var inside of function (turleGo) stands for the amount of total turtles walking with elves

--- Git Repository/Functions/Functions_Assignment/Functions _Wacky/js/script.js
+++ Git Repository/Functions/Functions_Assignment/Functions _Wacky/js/script.js
@@ -1,5 +1,11 @@
 //Functions Assignment - Wacky Week 4
 //Amy Harvey 12/20/2013
 
-var total = getAmts();
+var total = getAmts(); // first var for calling function
+
+function getAmts(e, t){
+    var turtleGo= prompt("How many Turtles are traveling with the Elves?");
+
+
+}
 
