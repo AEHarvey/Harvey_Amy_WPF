ec89f2c6c492b4011b8d7920e7b775a0a4a47e99
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-21 00:42:48 -0500

    added comments and var prompt for amount of elves going

--- Git Repository/Functions/Functions_Assignment/Functions _Wacky/js/script.js
+++ Git Repository/Functions/Functions_Assignment/Functions _Wacky/js/script.js
@@ -1,10 +1,11 @@
 //Functions Assignment - Wacky Week 4
 //Amy Harvey 12/20/2013
 
-var total = getAmts(); // first var for calling function
+var total = getAmts(); // arguments for first var for calling function
 
-function getAmts(e, t){
-    var turtleGo= prompt("How many Turtles are traveling with the Elves?");
+function getAmts(e, t){ //parameters
+    var turtleGo= prompt("How many Turtles are traveling with the Elves?"); // total amount of turtles traveling with elves
+    var elvesGo = prompt("How many Elves are traveling with the Turtles?"); // total amount of elves traveling with turtles
 
 
 }
