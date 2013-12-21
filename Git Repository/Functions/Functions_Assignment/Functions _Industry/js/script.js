6397249398841fb58ade2376c6ac2ed97260110c
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-20 22:13:08 -0500

    added elseif statement and console print, also comments submitted.

--- Git Repository/Functions/Functions_Assignment/Functions _Industry/js/script.js
+++ Git Repository/Functions/Functions_Assignment/Functions _Industry/js/script.js
@@ -6,7 +6,19 @@
 
 
 function graphicsAmt(){ //input function for the amount of graphics
-    var graphicsStore = prompt ("Enter the amount of items in you E-Store.");
+    var graphicsStore = prompt ("Enter the amount of Graphics in you E-Store.");
     var websitePages = prompt ("Enter the amount of pages you need.");
 
+        if(graphicsStore <= 10 && websitePages <= 5){
+            //if graphics needed is greater than less than 10 and website pages is less than 5 then print this "if" to console
+            console.log("I can make " + graphicsStore +  " graphics for you and " + websitePages + " , but I am not great at big number outputs");
+
+        }   else
+                if(websitePages <= 5 && graphicsStore >= 11){
+                   //if graphics are less than or equal to 5 and total graphics needed is greater than or equal to 11, print this to console
+
+
+
+
+                }
 }
