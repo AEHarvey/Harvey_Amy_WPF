1f16c3b87f4fc91afab90626f93841d1910f0b81
Amy Harvey <AEHarvey@fullsail.edu>
2013-12-20 22:33:30 -0500

    completion of industry specific problem, the amount of graphics needs for e-store and amount of pages needed

--- Git Repository/Functions/Functions_Assignment/Functions _Industry/js/script.js
+++ Git Repository/Functions/Functions_Assignment/Functions _Industry/js/script.js
@@ -4,22 +4,26 @@
 // industry section for assignment
 // webpages needed for a small shopping website
 
+var total = graphicsAmt();
 
-function graphicsAmt(){ //input function for the amount of graphics
-    var graphicsStore = prompt ("Enter the amount of Graphics in you E-Store.");
+function graphicsAmt(){
+    var graphicsStore = prompt ("Enter the amount of graphics in you E-Store.");// variable for circle size, diameter times pi, which are the arguments set in variable total
     var websitePages = prompt ("Enter the amount of pages you need.");
 
-        if(graphicsStore <= 10 && websitePages <= 5){
-            //if graphics needed is greater than less than 10 and website pages is less than 5 then print this "if" to console
-            console.log("I can make " + graphicsStore +  " graphics for you and " + websitePages + " , but I am not great at big number outputs");
+if(graphicsStore <= 10 && websitePages <= 5){
+    //code performed if amount of graphics is less than or equal to 10 and amount of pages is less than or equal to 5
+    console.log("I can make " + graphicsStore +  " graphics for you and " + websitePages + " website pages , but it will take sometime");
+    //
+    }else
+        if(websitePages <= 5 && graphicsStore >= 11){
+    //code performed if website pages is less than or equal to 5 but graphics is greater than or equal to 11
+    console.log("I can build " + websitePages + " website pages for you, but I cannot create " + graphicsStore + " graphics in time");
+    //what it will print "if" statement is true
+}       else{
 
-        }   else
-                if(websitePages <= 5 && graphicsStore >= 11){
-                   //if graphics are less than or equal to 5 and total graphics needed is greater than or equal to 11, print this to console
-                    console.log("I can build " + websitePages + " website pages for you, but I cannot create those " + graphicsStore + " graphics in time");
-                    //this is to printed to console if if statement is equal
+            //print this if bot conditions are false
+            console.log("I can't do either for you, the job is too great");
+                //all items are determined by prompts
+    }
+        }
 
-
-
-                }
-}
